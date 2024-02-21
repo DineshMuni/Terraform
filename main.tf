a0a0b64267c0603940680f881ff51557e5693e76
DineshMuni <157422280+DineshMuni@users.noreply.github.com>
2024-02-21 13:03:25 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  region                   = "eu-east-2"
+  region                   = "${AWS_REGION}"
   shared_credentials_files = ["/home/ec2-user/.aws/credentials"]
 }
 # Creating VPC
